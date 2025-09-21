9e92391e8f8e7a55e19c31e4853b440ec5b47a6c
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2025-09-21 13:18:36 +0530

    chore(app): add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
+import Home from './pages/home/home'
 
 function App() {
   return (
     <div className='h-screen w-screen overflow-y-auto overflow-x-hidden'>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/diagnosis" element={<h1>Diagnosis</h1>} />
+        <Route path="/contact" element={<h1>Contact Us</h1>} />
+      </Routes>
     </div>
   )
 }
