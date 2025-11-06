e3b41f7200db390e4c95587d162a0a46e182ac9c
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2025-11-07 01:20:08 +0530

    chore(constants): new route remedies

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,5 +3,6 @@ export const appName = "MS Diagnosis";
 export const navLinks = [
   { path: "/", label: "Home" },
   { path: "/diagnosis", label: "Diagnosis" },
-  { path: "/contact", label: "Contact" }
+  { path: "/remedies", label: "Remedies" },
+  { path: "/contact", label: "Contact" },
 ];
